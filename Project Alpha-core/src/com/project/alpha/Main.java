4d85b712ac5bdff027ba5962d9a8aec4632f957f
Loligesh <justus.fab@gmail.com>
2014-08-03 18:10:18 +0200

    stuff

---
+++ Project Alpha-core/src/com/project/alpha/Main.java
@@ -0,0 +1,42 @@
+package com.project.alpha;
+
+import com.badlogic.gdx.Screen;
+
+public class Main implements Screen {
+
+	@Override
+	public void render(float delta) {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	
+	}
+
+	@Override
+	public void show() {
+	
+	}
+
+	@Override
+	public void hide() {
+	
+	}
+
+	@Override
+	public void pause() {
+	
+	}
+
+	@Override
+	public void resume() {
+	
+	}
+
+	@Override
+	public void dispose() {
+	
+	}
+
+}

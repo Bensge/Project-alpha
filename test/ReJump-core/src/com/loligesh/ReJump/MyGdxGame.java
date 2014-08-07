8e8845edc2961e65e9c5c84b89a90a70a4bcc7df
Loligesh <justus.fab@gmail.com>
2014-08-07 18:59:28 +0200

    new project

---
+++ test/ReJump-core/src/com/loligesh/ReJump/MyGdxGame.java
@@ -0,0 +1,15 @@
+package com.loligesh.ReJump;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class MyGdxGame extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+
+	}
+}

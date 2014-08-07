3448266347a84b6d1a29ee5f88a2d375b82cd716
Benno <bensge@me.com>
2014-08-07 21:00:15 +0200

    Added player

---
+++ Project Alpha-core/src/com/project/alpha/Player.java
@@ -0,0 +1,13 @@
+package com.project.alpha;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Player extends Sprite {
+	
+	public void render(SpriteBatch batch)
+	{
+		
+	}
+}

439e0eeb19e1d40503cdea00d760b93a3a5e104b
Loligesh <justus.fab@gmail.com>
2014-08-07 21:47:57 +0200

    kein Ahnung

---
+++ Project Alpha-core/src/com/project/alpha/MainMenu.java
@@ -0,0 +1,55 @@
+package com.project.alpha;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+public class MainMenu implements Screen {
+
+	Main main;
+	
+	public MainMenu(Main main){
+		this.main = main;
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		if(Gdx.input.isTouched())
+			main.setScreen(new AlphaGame(main));
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	
+
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+}
--- Project Alpha-desktop/src/com/project/alpha/desktop/DesktopLauncher.java
+++ Project Alpha-desktop/src/com/project/alpha/desktop/DesktopLauncher.java
@@ -2,12 +2,12 @@ package com.project.alpha.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.project.alpha.MyGdxGame;
+import com.project.alpha.Main;
 
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new Main(), config);
 	}
 }

a753c4d8d4789641aee365d9bbec714cff9f1abc
Benno <bensge@me.com>
2014-12-07 17:11:20 +0100

    Now rendering the player

--- Project Alpha-core/src/com/project/GameStates/GameWorld.java
+++ Project Alpha-core/src/com/project/GameStates/GameWorld.java
@@ -7,38 +7,48 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.project.Entities.Player;
 
 public class GameWorld extends GameState {
 	
 	private OrthographicCamera camera;
 	private OrthogonalTiledMapRenderer renderer;
 	private TiledMap map;
-	//private Player player;
+	private Player player;
 
 	public GameWorld(GameStateManager manager) {
 		super(manager);
 		
+		//Camera
 		camera = new OrthographicCamera();
 		camera.viewportWidth = Gdx.graphics.getWidth();
 		camera.viewportHeight = Gdx.graphics.getHeight();
+		
+		//Map
 		map = new TmxMapLoader().load("maps/map.tmx");
+		
+		//Renderer
 		renderer = new OrthogonalTiledMapRenderer(map);
+		
+		//Player
+		player = new Player(10, 10);
+		
+		//Logging
 		System.out.println("camera: " + camera + " map: " + map + " renderer: " + renderer);
 	}
 
 	@Override
 	public void update(float delta) {
-		
+		player.update(delta);
 	}
 
 	@Override
 	public void render(SpriteBatch b) {
+		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		camera.position.set(20, 20, 0);
-		//camera.setToOrtho(true);
-		//camera.position.set(player.getX(), player.getY(), 0);
 		
 		camera.update();
 		renderer.setView(camera);
@@ -47,6 +57,7 @@ public class GameWorld extends GameState {
 		
 		renderer.getSpriteBatch().begin();
 		
+		player.draw(renderer.getSpriteBatch());
 		//player.draw(renderer.getSpriteBatch());
 		
 		renderer.getSpriteBatch().end();

1de4e326e31907755e7ebfd7aa3dc3bb78002bf6
Loligesh <justus.fab@gmail.com>
2014-12-07 17:26:48 +0100

    moving player fix & low physics

--- Project Alpha-core/src/com/project/Entities/Player.java
+++ Project Alpha-core/src/com/project/Entities/Player.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Player extends Entity {
 
-	float oldX, oldY;
+	float oldX, oldY, jump;
 	
 	public Player(int x, int y) {
 		super("img/enemy.png");
@@ -14,6 +14,7 @@ public class Player extends Entity {
 		setX(x);
 		setY(y);
 		
+		jump = 50;
 	}
 	
 	@Override
@@ -22,20 +23,36 @@ public class Player extends Entity {
 		oldX = getX();
 		oldY = getY();
 		
+		//velocity.x = 0;
+		velocity.y = 0;
+		
 		if(Gdx.input.isKeyPressed(Keys.A))
 			velocity.x = -10;
 		else if(Gdx.input.isKeyPressed(Keys.D))
 			velocity.x = 10;
 		
+		else{decreaseXVelocity();};
+		
 		if(Gdx.input.isKeyPressed(Keys.W))
-			velocity.y = -10;
-		else if(Gdx.input.isKeyPressed(Keys.S))
 			velocity.y = 10;
+		else if(Gdx.input.isKeyPressed(Keys.S)){
+			//go down a level
+			velocity.y -= 10;
+		}
+			
+		
 		
 		setX(getX() + velocity.x);
 		setY(getY() + velocity.y);
 	}
 	
+	private void decreaseXVelocity() {
+		if(velocity.x < 0)
+			velocity.x += 1;
+		else if(velocity.x > 0)
+			velocity.x -= 1;
+	}
+
 	@Override
 	public void render(SpriteBatch b) {
 		super.render(b);

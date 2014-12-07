0bd96ed7e32bf759601eb0fa52b9247d9d734350
Benno <bensge@me.com>
2014-12-07 16:31:36 +0100

    Added "abstract" classes

---
+++ Project Alpha-core/src/com/project/Entities/Entity.java
@@ -0,0 +1,7 @@
+package com.project.Entities;
+
+import Sprite;
+
+public class Entity extends Sprite {
+
+}
---
+++ Project Alpha-core/src/com/project/GameStates/GameWorld.java
@@ -0,0 +1,24 @@
+package com.project.GameStates;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameWorld extends GameState {
+
+	protected GameWorld(GameStateManager manager) {
+		super(manager);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(SpriteBatch b) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

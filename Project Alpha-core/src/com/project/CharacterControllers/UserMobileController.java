c526ce4908de8d6bc6ad17e553a03fc38f4b0924
Benno <bensge@me.com>
2014-12-16 12:47:02 +0100

    Actually let's support those touchy thingies

---
+++ Project Alpha-core/src/com/project/CharacterControllers/UserMobileController.java
@@ -0,0 +1,38 @@
+package com.project.CharacterControllers;
+
+import com.badlogic.gdx.Gdx;
+
+public class UserMobileController extends CharacterController {
+	
+	private Direction walkingDirection = Direction.None;
+
+	public UserMobileController(Character c) {
+		super(c);
+	}
+
+	@Override
+	public boolean shouldJump() {
+		return Gdx.input.isTouched();
+	}
+
+	@Override
+	public Direction walkDirection() {
+		return walkingDirection;
+	}
+
+	@Override
+	public void update() {
+		//As it turns out, a value of 0 of the accelerometer's Y axis means the device is in landscape right mode.
+		float accelY = Gdx.input.getAccelerometerY();
+		
+		final float ACCELEROMETER_THRESHOLD = 1.5f;
+		
+		if (accelY > -ACCELEROMETER_THRESHOLD && accelY < ACCELEROMETER_THRESHOLD)
+			walkingDirection = Direction.None;
+		else if (accelY <= -ACCELEROMETER_THRESHOLD)
+			walkingDirection = Direction.Right;
+		else if (accelY >= ACCELEROMETER_THRESHOLD)
+			walkingDirection = Direction.Left;
+	}
+
+}

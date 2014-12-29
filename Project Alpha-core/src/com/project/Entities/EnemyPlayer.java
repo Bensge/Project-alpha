057d842a1b3ba2a79b2151250317a3e4663fc574
Benno <bensge@me.com>
2014-12-29 16:20:37 +0100

    Added names on top of enemy players

--- Project Alpha-core/src/com/project/Entities/EnemyPlayer.java
+++ Project Alpha-core/src/com/project/Entities/EnemyPlayer.java
@@ -4,17 +4,24 @@ import java.awt.Point;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.project.constants.Constants;
 
-public class EnemyPlayer extends Entity{
+public class EnemyPlayer extends Entity
+{
+	private static BitmapFont nameFont = Constants.menlo10Font;
 
 	private String name;
+	private float nameLength;
 	private byte id;
-	
+
+
 	public EnemyPlayer(Point position, String name, byte id){
-		super(new Texture("img/player.png"),32,42);
+		super(new Texture("img/enemy.png"));
 		this.id = id;
 		this.name = name;
+		this.nameLength = nameFont.getBounds(name).width;
 		setBounds(position.x, position.y, getWidth(), getHeight());
 	}
 
@@ -29,6 +36,8 @@ public class EnemyPlayer extends Entity{
 	
 	public void render(Batch b){
 		super.render(b);
+
+		nameFont.draw(b,name,getX() + getWidth()/2 - nameLength/2,getY() + getHeight() + nameFont.getLineHeight());
 	}
 
 	@Override

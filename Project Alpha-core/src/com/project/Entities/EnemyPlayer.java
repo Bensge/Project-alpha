e38fe92ed726fc3e6921488a4af1b495a3ab2ab9
Benno <bensge@me.com>
2014-12-29 17:02:31 +0100

    Added experimental life bar

--- Project Alpha-core/src/com/project/Entities/EnemyPlayer.java
+++ Project Alpha-core/src/com/project/Entities/EnemyPlayer.java
@@ -6,16 +6,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.project.constants.Constants;
 
 public class EnemyPlayer extends Entity
 {
 	private static BitmapFont nameFont = Constants.menlo10Font;
+	private static ShapeRenderer lifeRenderer;
+	static {
+		lifeRenderer = new ShapeRenderer();
+	}
 
 	private String name;
 	private float nameLength;
 	private byte id;
-
 
 	public EnemyPlayer(Point position, String name, byte id){
 		super(new Texture("img/enemy.png"));
@@ -37,7 +41,22 @@ public class EnemyPlayer extends Entity
 	public void render(Batch b){
 		super.render(b);
 
-		nameFont.draw(b,name,getX() + getWidth()/2 - nameLength/2,getY() + getHeight() + nameFont.getLineHeight());
+		nameFont.draw(b,name,getX() + getWidth()/2 - nameLength/2,getY() + getHeight() + nameFont.getLineHeight() + 14);
+
+		/*
+		//Draw life bar
+		lifeRenderer.setProjectionMatrix(b.getProjectionMatrix());
+
+		lifeRenderer.begin(ShapeRenderer.ShapeType.Line);
+		lifeRenderer.setColor(1, 1, 1, 1);
+		lifeRenderer.rect(getX(), getY() + getHeight() + 2, getWidth(), 5);
+		lifeRenderer.end();
+
+		lifeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		lifeRenderer.setColor(1, 1, 1, 1);
+		lifeRenderer.rect(getX(), getY() + getHeight() + 2, getWidth() * (float)life / 100.f, 5);
+		lifeRenderer.end();
+		*/
 	}
 
 	@Override

7365784f7e1b9d9fb87bbe48d8b540e51b80875c
Loligesh <justus.fab@gmail.com>
2014-12-29 21:10:05 +0100

    improved some values

--- Project Alpha-core/src/com/project/Entities/Bullet.java
+++ Project Alpha-core/src/com/project/Entities/Bullet.java
@@ -2,7 +2,7 @@ package com.project.Entities;
 
 public class Bullet extends Projectile{
 
-	public final static float speed = 500;
+	public final static float speed = 600;
 	public final static float explosionRadius = 0;
 	
 	public Bullet(String s, float targetX, float targetY, float originX, float originY, byte ownerID, boolean isMyOwn){
